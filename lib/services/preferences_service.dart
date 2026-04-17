474da032c701790e3f55ffe956850177be960690
lbaum <lbaumgaertner@culba.de>
2026-04-17 23:21:03 +0700

    feat: persist delay setting across app restarts

--- lib/services/preferences_service.dart
+++ lib/services/preferences_service.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class PreferencesService {
   static const String _lastDurationKey = 'last_duration';
+  static const String _prepTimeKey = 'prep_time';
 
   final SharedPreferences _prefs;
 
@@ -19,5 +20,12 @@ class PreferencesService {
   Future<void> saveLastDuration(int duration) async {
     await _prefs.setInt(_lastDurationKey, duration);
   }
-}
 
+  int getLastPrepTime() {
+    return _prefs.getInt(_prepTimeKey) ?? 0;
+  }
+
+  Future<void> saveLastPrepTime(int seconds) async {
+    await _prefs.setInt(_prepTimeKey, seconds);
+  }
+}
