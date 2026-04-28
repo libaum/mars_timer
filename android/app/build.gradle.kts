b497631c09c10b28e57557c1f7f948cc59ddb1d5
lbaum <lbaumgaertner@culba.de>
2026-04-28 16:52:24 +0700

    chore: enable R8 minification and resource shrinking

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,8 +48,12 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,9 @@
+# Flutter
+-keep class io.flutter.** { *; }
+-keep class io.flutter.plugins.** { *; }
+
+# audioplayers
+-keep class xyz.luan.audioplayers.** { *; }
+
+# sqflite
+-keep class com.tekartik.sqflite.** { *; }
